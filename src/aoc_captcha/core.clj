be31bd7a168af37f6f7e0288d1f4e6c7f89f4b13
Eric Coleman <eric@epiccoleman.com>
2017-12-09 23:19:14 -0500

    do some refactors, test the only thing that really matters

--- src/aoc_captcha/core.clj
+++ src/aoc_captcha/core.clj
@@ -1,6 +1,7 @@
 (ns aoc-captcha.core)
 
-(defn char->digit [char] (Integer/parseInt (str char)))
+(defn char->digit [char]
+  (Integer/parseInt (str char)))
 
 (defn str->digit-list [str] (map char->digit (seq str)))
 
@@ -9,23 +10,23 @@
 
 (defn ends-match? [list] (= (first list) (last list)))
 
-(defn captcha-count-to-end [captcha]
-  (loop [captcha (str->digit-list captcha)
+(defn captcha-count-to-end [captcha-str]
+  (loop [captcha-list (str->digit-list captcha-str)
          result 0]
-    (if (<= 2 (count captcha))
-      (recur (rest captcha)
+    (if (<= 2 (count captcha-list))
+      (recur (rest captcha-list)
              (+ result
-                (if (first-two-match? captcha)
-                  (first captcha)
+                (if (first-two-match? captcha-list)
+                  (first captcha-list)
                   0)))
       result)))
 
-(defn captcha-count-last-element [captcha]
-  (let [captcha (str->digit-list captcha)]
-    (if (ends-match? captcha)
-      (last captcha)
+(defn captcha-count-last-element [captcha-str]
+  (let [captcha-list (str->digit-list captcha-str)]
+    (if (ends-match? captcha-list)
+      (last captcha-list)
       0)))
 
-(defn captcha-solve [captcha]
-  (+ (captcha-count-to-end captcha)
-     (captcha-count-last-element captcha)))
+(defn captcha-solve [captcha-str]
+  (+ (captcha-count-to-end captcha-str)
+     (captcha-count-last-element captcha-str)))
